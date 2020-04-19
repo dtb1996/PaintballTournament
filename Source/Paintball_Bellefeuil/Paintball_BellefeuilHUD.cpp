873293b62f189e4257dd9b1f55420a3d158ac1b3
dbellefeuille <dtb1996@yahoo.com>
2020-04-18 22:32:11 -0400

    Added new HUD elements

--- Source/Paintball_Bellefeuil/Paintball_BellefeuilHUD.cpp
+++ Source/Paintball_Bellefeuil/Paintball_BellefeuilHUD.cpp
@@ -25,7 +25,7 @@ void APaintball_BellefeuilHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
+	const FVector2D CrosshairDrawPosition( (Center.X - 10.0f),
 										   (Center.Y + 20.0f));
 
 	// draw the crosshair
